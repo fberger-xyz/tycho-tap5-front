3222c1c45de9726295d9c8c99f2bf363e5c33db1
fberger <fberger@fbergers-MacBook-Pro.local>
2025-06-06 12:20:41 +0200

    chore: setup

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -8,13 +8,19 @@ import { DefaultColors } from 'tailwindcss/types/generated/colors'
 
 /**
  * meta
+ * https://github.com/propeller-heads/tycho-x/blob/main/TAP-5.md
  */
 
-export const SITE_NAME = 'Charts'
+export const SITE_NAME = 'Tycho Market Maker'
 export const IS_DEV = process.env.NODE_ENV === 'development'
-export const SITE_DOMAIN = IS_DEV ? 'http://localhost:3000' : `https://${SITE_NAME.toLowerCase()}.fberger.xyz`
+export const SITE_DOMAIN = IS_DEV ? 'http://localhost:3000' : `https://${SITE_NAME.replaceAll(' ', '-').toLowerCase()}.fberger.xyz`
 export const SITE_URL = SITE_DOMAIN.replace('www.', '')
-export const APP_METADATA = { SITE_NAME, SITE_DOMAIN, SITE_DESCRIPTION: 'Charts tooling [TBA]', SITE_URL: SITE_URL }
+export const APP_METADATA = {
+    SITE_NAME,
+    SITE_DOMAIN,
+    SITE_DESCRIPTION: 'A market maker that anyone can easily run, well documented',
+    SITE_URL: SITE_URL,
+}
 
 /**
  * pages
